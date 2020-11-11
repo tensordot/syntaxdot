42f6ac193b01de6ea9bc1c00ef0372cf9acef0e5
Daniël de Kok <me@danieldk.eu>
2020-11-11 13:57:14 +0100

    Ingest edit_tree crate in syntaxdot-encoders
    - The crate is small and this allows us to make changes without
      waiting for crates.io uploads.
    - Remove code that we do not need in syntaxdot-encoders.
    - Some small cleanups.

--- syntaxdot-encoders/src/lemma/mod.rs
+++ syntaxdot-encoders/src/lemma/mod.rs
@@ -1,7 +1,10 @@
 use thiserror::Error;
 
+mod encoder;
+pub use self::encoder::{BackoffStrategy, EditTreeEncoder};
+
 mod edit_tree;
-pub use self::edit_tree::{BackoffStrategy, EditTreeEncoder};
+pub use edit_tree::EditTree;
 
 mod tdz;
 pub use tdz::TdzLemmaEncoder;
@@ -12,4 +15,8 @@ pub enum EncodeError {
     /// The token does not have a lemma.
     #[error("token without a lemma: '{form:?}'")]
     MissingLemma { form: String },
+
+    /// No edit tree can be constructed.
+    #[error("cannot find an edit tree that rewrites '{form:?}' into '{lemma:?}'")]
+    NoEditTree { form: String, lemma: String },
 }
