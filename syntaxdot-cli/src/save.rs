344c9a6949a7b86081efa54d3c0cd3acb994703f
Daniël de Kok <me@danieldk.eu>
2020-11-11 13:15:36 +0100

    Remove unused savers

--- syntaxdot-cli/src/save.rs
+++ syntaxdot-cli/src/save.rs
@@ -1,5 +1,3 @@
-use std::marker::PhantomData;
-
 use anyhow::{Context, Result};
 use tch::nn::VarStore;
 
@@ -30,39 +28,6 @@ pub trait Save<P> {
     /// actual measure can be wrapped in `std::cmp::Reverse` to
     /// reverse the ordering.
     fn save(&mut self, vs: &VarStore, completed: CompletedUnit<P>) -> Result<()>;
-}
-
-/// Save epochs.
-#[derive(Clone)]
-pub struct EpochSaver<P> {
-    epoch: usize,
-    prefix: String,
-    _phantom: PhantomData<P>,
-}
-
-impl<P> EpochSaver<P> {
-    pub fn new(prefix: impl Into<String>) -> Self {
-        EpochSaver {
-            prefix: prefix.into(),
-            epoch: 0,
-            _phantom: PhantomData,
-        }
-    }
-}
-
-impl<P> Save<P> for EpochSaver<P> {
-    fn save(&mut self, vs: &VarStore, completed: CompletedUnit<P>) -> Result<()> {
-        if let CompletedUnit::Epoch(_) = completed {
-            vs.save(format!("{}epoch-{}", self.prefix, self.epoch))
-                .context(format!(
-                    "Cannot save variable store for epoch {}",
-                    self.epoch
-                ))?;
-            self.epoch += 1;
-        }
-
-        Ok(())
-    }
 }
 
 /// Save best epochs with the best performance so far.
@@ -113,65 +78,6 @@ where
             }
 
             self.epoch += 1;
-        }
-
-        Ok(())
-    }
-}
-
-/// Save every epoch and N batches.
-#[derive(Clone)]
-pub struct EpochAndBatchesSaver<P> {
-    batch: usize,
-    epoch: usize,
-    epoch_batch: usize,
-    n_batches: usize,
-    prefix: String,
-    _phantom: PhantomData<P>,
-}
-
-impl<P> EpochAndBatchesSaver<P> {
-    /// Construct a saver that saves every epoch and N batches.
-    pub fn new(prefix: impl Into<String>, n_batches: usize) -> Self {
-        EpochAndBatchesSaver {
-            batch: 0,
-            epoch: 0,
-            epoch_batch: 0,
-            n_batches,
-            prefix: prefix.into(),
-            _phantom: PhantomData,
-        }
-    }
-}
-
-impl<P> Save<P> for EpochAndBatchesSaver<P> {
-    fn save(&mut self, vs: &VarStore, completed: CompletedUnit<P>) -> Result<()> {
-        match completed {
-            CompletedUnit::Epoch(_) => {
-                vs.save(format!("{}epoch-{}", self.prefix, self.epoch))
-                    .context(format!(
-                        "Cannot save variable store for epoch {}",
-                        self.epoch
-                    ))?;
-
-                self.epoch += 1;
-                self.epoch_batch = 0;
-            }
-            CompletedUnit::Batch(_) => {
-                if (self.batch + 1) % self.n_batches == 0 {
-                    vs.save(format!(
-                        "{}epoch-{}-batch-{}",
-                        self.prefix, self.epoch, self.epoch_batch
-                    ))
-                    .context(format!(
-                        "Cannot save variable store for epoch {} batch {}",
-                        self.epoch, self.batch
-                    ))?;
-                }
-
-                self.batch += 1;
-                self.epoch_batch += 1;
-            }
         }
 
         Ok(())
