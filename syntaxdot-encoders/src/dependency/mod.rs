396fed598b3b13df26b01e5ff175a8276128913e
Daniël de Kok <me@danieldk.eu>
2021-01-20 11:30:52 +0100

    Add an encoder/decoder for biaffine parsing

--- syntaxdot-encoders/src/dependency/mod.rs
+++ syntaxdot-encoders/src/dependency/mod.rs
@@ -1,1 +1,8 @@
-pub mod mst;
+//! Dependency encoding/decoding for biaffine parsing.
+
+mod encoder;
+pub use encoder::{
+    DependencyEncoding, EncodeError, ImmutableDependencyEncoder, MutableDependencyEncoder,
+};
+
+mod mst;
