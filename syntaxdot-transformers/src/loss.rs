f61548dd9a61f6ae3930f9ce99551aae3c12658c
Daniël de Kok <me@danieldk.eu>
2021-02-22 12:04:29 +0100

    Add label smoothing to dependency head targets
    This change also enables label smoothing for dependency head targets.

--- syntaxdot-transformers/src/loss.rs
+++ syntaxdot-transformers/src/loss.rs
@@ -1,5 +1,6 @@
+use tch::{Kind, Reduction, Tensor};
+
 use crate::TransformerError;
-use tch::{Reduction, Tensor};
 
 trait Reduce {
     type Error;
@@ -46,9 +47,18 @@ impl CrossEntropyLoss {
     /// Compute the cross-entropy loss.
     ///
     /// `logits` should be the unnormalized probablilities of shape
-    /// `[batch_size, seq_len]` and `targets` the gold-standard labels
+    /// `[batch_size, n_classes]` and `targets` the gold-standard labels
     /// with shape `[batch_size]`.
-    pub fn forward(&self, logits: &Tensor, targets: &Tensor) -> Result<Tensor, TransformerError> {
+    ///
+    /// The optional target mask has to be of shape `[batch_size, n_classes]`.
+    /// If the mask is not provided, then all `n_classes` will be used in
+    /// label smoothing.
+    pub fn forward(
+        &self,
+        logits: &Tensor,
+        targets: &Tensor,
+        target_mask: Option<&Tensor>,
+    ) -> Result<Tensor, TransformerError> {
         let (_, n_classes) = logits.size2()?;
         let log_probs = logits.f_log_softmax(-1, logits.kind())?;
 
@@ -61,13 +71,30 @@ impl CrossEntropyLoss {
                     targets.f_where3(&targets.f_ne(self.ignore_index)?, 0)?;
 
                 // Set all labels to label_smoothing and the target to 1-label_smoothing.
-                let smoothed_targets = tch::no_grad(|| {
-                    Tensor::f_full_like(&log_probs, label_smoothing / (n_classes - 1) as f64)?
-                        .f_scatter1(
-                            1,
-                            &targets_non_negative.f_unsqueeze(1)?,
-                            1. - label_smoothing,
-                        )
+                let smoothed_targets = tch::no_grad(|| match target_mask {
+                    None => {
+                        Tensor::f_full_like(&log_probs, label_smoothing / (n_classes - 1) as f64)?
+                            .f_scatter1(
+                                1,
+                                &targets_non_negative.f_unsqueeze(1)?,
+                                1. - label_smoothing,
+                            )
+                    }
+                    Some(target_mask) => {
+                        let batch_probs = label_smoothing
+                            / target_mask.f_sum1(&[-1], false, Kind::Float)?.f_sub1(1)?;
+                        Tensor::f_zeros_like(&log_probs)?
+                            // Set label probabilities to batch smoothing probability.
+                            .f_add_(&batch_probs.f_unsqueeze(-1)?)?
+                            // Mask out padding.
+                            .f_mul(&target_mask.to_kind(Kind::Float))?
+                            // Assign probabilities to gold standard labels.
+                            .f_scatter1(
+                                1,
+                                &targets_non_negative.f_unsqueeze(1)?,
+                                1. - label_smoothing,
+                            )
+                    }
                 })?;
                 let losses = (smoothed_targets.f_neg()?.f_mul(&log_probs)?).f_sum1(
                     &[-1],
@@ -102,7 +129,7 @@ mod tests {
         let logits = Tensor::of_slice(&[-1., -1., 1., -1., -1.]).view([1, 5]);
         let targets = Tensor::of_slice(&[2i64]).view([1]);
         let cross_entropy_loss = CrossEntropyLoss::new(-1, None, Reduction::None);
-        let loss: ArrayD<f32> = (&cross_entropy_loss.forward(&logits, &targets).unwrap())
+        let loss: ArrayD<f32> = (&cross_entropy_loss.forward(&logits, &targets, None).unwrap())
             .try_into()
             .unwrap();
 
@@ -114,7 +141,21 @@ mod tests {
         let logits = Tensor::of_slice(&[-1., -1., 1., -1., -1.]).view([1, 5]);
         let targets = Tensor::of_slice(&[2i64]).view([1]);
         let cross_entropy_loss = CrossEntropyLoss::new(-1, Some(0.1), Reduction::None);
-        let loss: ArrayD<f32> = (&cross_entropy_loss.forward(&logits, &targets).unwrap())
+        let loss: ArrayD<f32> = (&cross_entropy_loss.forward(&logits, &targets, None).unwrap())
+            .try_into()
+            .unwrap();
+        assert_abs_diff_eq!(loss, array![0.632653].into_dyn(), epsilon = 1e-6);
+    }
+
+    #[test]
+    fn cross_entropy_with_label_smoothing_and_mask() {
+        let logits = Tensor::of_slice(&[-1., -1., 1., -1., -1.]).view([1, 5]);
+        let target_mask = Tensor::of_slice(&[true, false, true, false, true]).view([1, 5]);
+        let targets = Tensor::of_slice(&[2i64]).view([1]);
+        let cross_entropy_loss = CrossEntropyLoss::new(-1, Some(0.1), Reduction::None);
+        let loss: ArrayD<f32> = (&cross_entropy_loss
+            .forward(&logits, &targets, Some(&target_mask))
+            .unwrap())
             .try_into()
             .unwrap();
         assert_abs_diff_eq!(loss, array![0.632653].into_dyn(), epsilon = 1e-6);
